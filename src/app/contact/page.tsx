a75b0bcc277aa339a1347edb272a2fd2f2b66376
KiranShoaib <kiranshoaib091@gmail.com>
2024-11-04 11:27:53 +0500

    set the contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,25 +10,25 @@ function Contact(){
             <div>
                 <h1 className="pt-8 md:pt-32 text-[#d58952] text-3xl md:text-4xl font-semibold text-center md:text-start">Get in Touch</h1>
                 <p className="text-white pt-2 text-center md:pr-32">Have questions or need assistance? Reach out to us, and we&#39;ll be happy to help make your experience seamless</p>
-                <div className="container pt-4 flex flex-col justify-center items-center" >
+                <div className="container flex flex-col justify-center items-center" >
                 <div className="md:mr-36">
                     <FaMapMarkerAlt className="text-[#d58952] bg-white w-[30px] h-[30px] rounded-full border-white border-[5px] -translate-x-12 translate-y-9"/>
                     <h3 className="text-[#d58952] font-semibold text-lg">Address</h3>
                     <p className="text-white">Jamshed Road, Karachi</p>
                 </div>
-                <div className="pt-2 pr-8 md:mr-36">
+                <div className="pr-11 md:mr-36">
                     <FaPhoneAlt className="text-[#d58952] bg-white w-[30px] h-[30px] rounded-full border-white border-[5px] -translate-x-12 translate-y-9"/>
                     <h3 className="text-[#d58952] font-semibold text-lg">Phone</h3>
                     <p className="text-white">+92-321-1836917</p>
                 </div>
-                <div className="pt-2 pl-8 md:mr-36">
+                <div className="pl-8 md:mr-36">
                     <TiMail className="text-[#d58952] bg-white w-[30px] h-[30px] rounded-full border-white border-[5px] -translate-x-12 translate-y-9"/>
                     <h3 className="text-[#d58952] font-semibold text-lg">Email</h3>
                     <p className="text-white">kiranshoaib091@gmail.com</p>
                 </div>
                 </div>
             </div>
-            <div className="contactForm mt-8 md:mt-60 mx-auto p-5 md:p-7 w-[90%] h-[300px] sm:w-[60%] md:w-[70%] bg-white md:mr-24">
+            <div className="contactForm mt-5 md:mt-60 mx-auto p-5 md:p-7 w-[90%] h-[300px] sm:w-[60%] md:w-[70%] bg-white md:mr-24">
                 <form>
                     <h2 className="text-[#d58952] text-lg font-semibold">Send Message</h2>
                     <div className="inputBox relative mt-3 ">
